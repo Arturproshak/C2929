72a3a5a384351741c34c2d5974a157cd2a7a9ca7
Artur <arturpro20091@gmail.com>
2023-01-26 18:55:41 +0200

    Version 0.1

---
+++ main.py
@@ -0,0 +1,13 @@
+class Car:
+    wheels = 4
+    speed = 120
+    color= 'Green'
+    power = 85
+
+
+print(Car.wheels)
+print(Car.speed)
+Car.speed = 160
+print(Car.speed)
+Car.power = 105
+print(Car.power)
